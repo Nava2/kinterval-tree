5f498216e8e213b3bf10ad2596cce2d8634202a8
Kevin Brightwell <kevin.brightwell2@gmail.com>
2023-07-31 16:49:23 -0400

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ import io.gitlab.arturbosch.detekt.DetektCreateBaselineTask
 import org.jetbrains.kotlin.gradle.plugin.mpp.pm20.util.archivesName
 
 // update plugins block, too
-val kotlinVersion = "1.8.22"
+val detektVersion = "1.23.0"
 val detektVersion = "1.23.1"
 val junitVersion = "5.9.3"
 
